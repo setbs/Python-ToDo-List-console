d17b91dc25038dd749024db9c7bdebd7fbc74ab2
ew <ew@ew.ew>
2024-11-25 19:47:35 +0100

    Added simple DataBase using .txt file

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case "add":
-            todo = input("Enter TODO: ")
+            todo = input("Enter TODO: ") + "\n"
+
+            file = open('files/todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('files/todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case "show":
+            file = open('files/todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
